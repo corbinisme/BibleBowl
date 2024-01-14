a30e7718706437cc0c4303abc176b345136fc983
Corbin Rose <corbin_rose@ucg.org>
2024-01-14 18:03:58 -0500

    trying to finish the game

--- src/Components/RandomQuestion.js
+++ src/Components/RandomQuestion.js
@@ -2,12 +2,43 @@ import { useEffect, useState } from "react";
 import Question from "./Question";
 function RandomQuestion(props){
 
+    const st = window.localStorage;
     const data= props.data;
+    const [completed, setCompleted] = useState(false);
     const [randomIndex, setrandomIndex] = useState(1);
+    const [usedQuestions, setUsedQuestions] = useState([st.getItem("usedQuestions")? st.getItem("usedQuestions").split(","): []]);
 
+
+    const generateRandom = () => {
+        let random = Math.floor(Math.random() * data.length);
+
+        // keep making a random number until it is not in the usedQuestions array
+        while(usedQuestions.includes(random)){
+            // check to see if all questions have been used
+            if(usedQuestions.length == data.length){
+                // reset the usedQuestions array
+                setUsedQuestions([]);
+                st.removeItem("usedQuestions");
+                setCompleted(true);
+                break;
+            }
+            random = Math.floor(Math.random() * data.length);
+            
+        }
+        if(completed){
+            alert("All questions have been used. Winner winner chicken dinner!");
+            
+            
+        } else {
+            setrandomIndex(random);
+            setUsedQuestions([...usedQuestions, random]);
+            st.setItem("usedQuestions", usedQuestions.join(","));
+        }
+        
+    }
     useEffect(() => {
-
-        setrandomIndex(Math.floor(Math.random() * data.length));
+        // make sure it has not been used in this session
+        generateRandom();
     }, [data])
 
     const givePoints = props.givePoints;
@@ -37,7 +68,7 @@ function RandomQuestion(props){
                         </div>
                         <div className="col-sm-4 text-center">
                             <button className="btn btn-primary" 
-                                onClick={() => setrandomIndex(Math.floor(Math.random() * data.length))}>Next Question
+                                onClick={() => generateRandom()}>Next Question
                             </button>
                         </div>
                         <div className="col-sm-4 text-end">
